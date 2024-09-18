2a469b63072fabd2fd7aa5d7544fa259ee80ae7d
abdisetiakawan <bedikadiryt@gmail.com>
2024-09-18 08:33:32 +0700

    "Added @heroicons/react and @material-tailwind/react dependencies, updated package-lock.json, and added Header component to App.jsx"

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div className="bg-slate-200">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold">Auth Sederhana</h1>
+        <ul>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
