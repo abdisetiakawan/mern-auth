94722cd23912d65f4de47f8698102c2d29e6d24f
abdisetiakawan <bedikadiryt@gmail.com>
2024-09-25 22:42:30 +0700

    "Updated server/index.js to import additional routers, configure dotenv, and modify app setup and listening"

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import connectDB from "./db/index.js";
-import router from "./routes/user.route.js";
+import testRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.controller.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
+
 connectDB();
 
-app.get("/test", router);
+app.use("/test", testRouter);
+app.use("/api/auth", authRouter);
 
-app.listen(3000, () => {
+app.use(express.json());
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server listening to http://localhost:3000`);
 });
