359227bdd444f65e8d45cfc9cf4c20c300c3fb22
abdisetiakawan <bedikadiryt@gmail.com>
2024-09-25 21:53:10 +0700

    "Added dotenv, mongoose, and connectDB functionality to the project, updated package.json and package-lock.json, and modified server/index.js to include connectDB call."

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
+import connectDB from "./db/index.js";
 const app = express();
-
+connectDB();
 app.listen(3000, () => {
   console.log(`Server listening to http://localhost:3000`);
 });
