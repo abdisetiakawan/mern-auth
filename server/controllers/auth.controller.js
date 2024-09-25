f8fc8b7a46d180e03c88a755ed066abf607f21d8
abdisetiakawan <bedikadiryt@gmail.com>
2024-09-25 23:16:09 +0700

    "Updated signUp function in auth.controller.js to check for existing users by email or username, and to return a sanitized user object in the response."

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,28 +1,40 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
   try {
     const { username, password, email } = req.body;
+
     if (!username || !password || !email) {
       return res
         .status(400)
         .json({ message: "Please provide username, password, and email" });
     }
 
-    const existingUser = await User.findOne({ where: { username } });
+    const existingUser = await User.findOne({
+      $or: [{ email }, { username }],
+    });
     if (existingUser) {
-      return res.status(400).json({ message: "Username already exists" });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
       username,
-      password: hashedPassword,
       email,
+      password: hashedPassword,
     });
-    res.status(201).json({ message: "User registered successfully", newUser });
+
+    await newUser.save();
+
+    const userResponse = newUser.toObject();
+    delete userResponse.password;
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: userResponse });
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
