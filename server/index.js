108441748f91e567b3a04a863323e44c488e71f6
abdisetiakawan <bedikadiryt@gmail.com>
2024-09-25 22:25:22 +0700

    "Relocate User model and added user route test to server"

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import connectDB from "./db/index.js";
+import router from "./routes/user.route.js";
 const app = express();
 connectDB();
+
+app.get("/test", router);
+
 app.listen(3000, () => {
   console.log(`Server listening to http://localhost:3000`);
 });
