dd06704d2a3476903f83366f8eb874050c03c3e8
abdisetiakawan <bedikadiryt@gmail.com>
2024-09-25 23:07:52 +0700

    "Added 404 error handling and error handler middleware to server"

--- server/index.js
+++ server/index.js
@@ -15,3 +15,20 @@ connectDB();
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server listening to http://localhost:3000`);
 });
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // Send error response
+  res.status(err.status || 500).send({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {},
+  });
+});
