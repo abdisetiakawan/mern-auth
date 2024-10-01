2667000e1e710cf6e840852d7ea89cda5bf127c0
abdisetiakawan <bedikadiryt@gmail.com>
2024-10-01 23:19:06 +0700

    "Added form handling and API call to SignUp page, and configured proxy in vite.config.js"

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,6 +3,22 @@ import { Link } from "react-router-dom";
 
 export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/auth/sign-up", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+  };
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -11,18 +27,20 @@ export default function SignUp() {
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-bold my-10">Sign Up</h1>
-      <form action="" className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} action="" className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Email"
           id="email"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="Username"
           id="username"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <div className="relative">
           <input
@@ -30,6 +48,7 @@ export default function SignUp() {
             placeholder="Password"
             id="password"
             className="bg-slate-100 p-3 rounded-lg w-full"
+            onChange={handleChange}
           />
           <button
             type="button"
