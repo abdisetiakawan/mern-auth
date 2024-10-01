50d7f989257363921fdbd927fc3158c0a619e3bf
abdisetiakawan <bedikadiryt@gmail.com>
2024-10-01 22:43:44 +0700

    "Added Font Awesome stylesheet to index.html and implemented SignUp page with form fields and password visibility toggle"

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,54 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function SignUp() {
-  return <div>SignUp</div>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-bold my-10">Sign Up</h1>
+      <form action="" className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <div className="relative">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            id="password"
+            className="bg-slate-100 p-3 rounded-lg w-full"
+          />
+          <button
+            type="button"
+            onClick={togglePasswordVisibility}
+            className="absolute inset-y-0 right-3 flex items-center"
+          >
+            <i className={showPassword ? "fas fa-eye-slash" : "fas fa-eye"}></i>
+          </button>
+        </div>
+        <button className="bg-slate-800 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-3 mt-4">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-500">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
